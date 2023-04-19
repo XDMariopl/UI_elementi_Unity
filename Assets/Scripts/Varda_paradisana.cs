bfee5bfba0371a01d35981e876fbd90f3a07c742
Maris Klaiva <maris.klaiva@gmail>
2023-04-19 12:21:07 +0300

    text field,input,button

---
+++ Assets/Scripts/Varda_paradisana.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Varda_paradisana : MonoBehaviour {
+	public int frameSK = 0;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Uzsakta programa! tu esi"+frameSK+" kadra");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Kadrs: " + frameSK);
+		frameSK++;
+	}
+}
