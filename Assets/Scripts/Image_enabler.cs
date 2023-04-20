d4f3c17aff5a4724a4f3dedfcbe2ac35ac4defde
Maris Klaiva <maris.klaiva@gmail>
2023-04-20 12:16:04 +0300

    Toggle, Dropdown, Images

---
+++ Assets/Scripts/Image_enabler.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Image_enabler : MonoBehaviour {
+	public GameObject beanImg;
+	public GameObject lacisImg;
+	public GameObject omeImg;
+	public GameObject masinaImg;
+    public GameObject SusImg;
+    public GameObject paKreisiPoga;
+    public GameObject paLabiPoga;
+    public GameObject VariableImg;
+    public Sprite[] ImgMas;
+	private bool vertiba;
+
+    public void dropdown(int skaitlis)
+    {
+        switch(skaitlis)
+        {
+            case 0:
+                VariableImg.GetComponent<Image>().sprite = ImgMas[0];
+                break;
+
+            case 1:
+                VariableImg.GetComponent<Image>().sprite = ImgMas[1];
+                break;
+
+            case 2:
+                VariableImg.GetComponent<Image>().sprite = ImgMas[2];
+                break;
+
+            default:
+                VariableImg.GetComponent<Image>().sprite = ImgMas[skaitlis];
+                break;
+        }
+    }
+	
+	public void BeanImgOnOff(bool vertiba)
+	{
+		beanImg.SetActive(vertiba);
+        paKreisiPoga.GetComponent<Toggle>().interactable= vertiba;
+        paLabiPoga.GetComponent <Toggle>().interactable= vertiba;
+	}
+
+    public void paKreisiBean()
+    {
+        beanImg.transform.localScale =new Vector2(2, 2);
+    }
+
+    public void paLabiBean()
+    {
+        beanImg.transform.localScale=new Vector2(-2, 2);
+    }
+    public void LacisImgOnOff(bool vertiba)
+    {
+        lacisImg.SetActive(vertiba);
+    }
+    public void OmeImgOnOff(bool vertiba)
+    {
+        omeImg.SetActive(vertiba);
+    }
+    public void MasinaImgOnOff(bool vertiba)
+    {
+        masinaImg.SetActive(vertiba);
+    }
+    public void SusImgOnOff(bool vertiba)
+    {
+        SusImg.SetActive(vertiba);
+    }
+}
--- Assets/Scripts/Varda_paradisana.cs
+++ Assets/Scripts/Varda_paradisana.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Varda_paradisana : MonoBehaviour {
-	public int frameSK = 0;
+	//public int frameSK = 0;
+	private string txt;
+	public GameObject inputfield;
+	public GameObject TextField;
+	private string[] fragmenti = {"kys right now!","Ha funny name.","What do u need?" };
+	int indekss;
+
+	public void holdTxt()
+	{
+		indekss= Random.Range(0,fragmenti.Length);
+		txt=inputfield.GetComponent<InputField>().text;
+		if (txt == "")
+		{
+			TextField.GetComponent<Text>().text = "Enter ur name u dumb creature";
+		}
+		else
+		{
+			TextField.GetComponent<Text>().text = fragmenti[indekss] + " " + txt.ToUpper();
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log("Uzsakta programa! tu esi"+frameSK+" kadra");
+		//Debug.Log("Uzsakta programa! tu esi"+frameSK+" kadra");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log("Kadrs: " + frameSK);
-		frameSK++;
+		//Debug.Log("Kadrs: " + frameSK);
+		//frameSK++;
 	}
 }
